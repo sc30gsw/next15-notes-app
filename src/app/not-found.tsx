1ae66687872cc5a6d080ab8ca1b017f1b50af21e
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-12-30 15:51:25 +0900

    💄 not-foundの作成

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
+import { Heading } from '@/components/justd/ui'
 import Link from 'next/link'
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center gap-2">
+      <Heading>Not Found</Heading>
+      <Heading level={3}>Could not find requested resource</Heading>
+      <Link href="/" className="text-blue-500 hover:underline">
+        Return Home
+      </Link>
     </div>
   )
 }
